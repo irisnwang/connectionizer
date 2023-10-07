fa16a9af8bb94c6999b1cdd8eac893143b7165b4
Iris <wang.ir@northeastern.edu>
2023-10-07 17:40:51 -0400

    prep for deploy

--- src/services/puzzles-service.js
+++ src/services/puzzles-service.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
-const PUZZLES_API = 'http://localhost:4000/api/puzzles';
-const PUZZLE_IDS_API = 'http://localhost:4000/api/puzzleIds'
+const API_BASE = process.env.REACT_APP_PUZZLE_API || "http://localhost:4000/api";
+const PUZZLES_API = `${API_BASE}/puzzles`
+const PUZZLE_IDS_API = `${API_BASE}/puzzleIds`
 
 export const createPuzzle = async (puzzle) => {
     const response = await axios.post(PUZZLES_API, puzzle)
